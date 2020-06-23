dbcbdad26701c0aaee99facc1ca7d7b14fd96821
yehuda <>
2020-06-23 17:44:37 +0300

    added parcer

--- app/src/main/java/com/example/exerciseone/Contact.java
+++ app/src/main/java/com/example/exerciseone/Contact.java
@@ -1,6 +1,9 @@
 package com.example.exerciseone;
 
-public class Contact {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Contact implements Parcelable {
     String mName;
     String mPhoneNumber;
     String mEmail;
@@ -12,4 +15,36 @@ public class Contact {
         mEmail = email;
         mImage = image;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.mName);
+        dest.writeString(this.mPhoneNumber);
+        dest.writeString(this.mEmail);
+        dest.writeInt(this.mImage);
+    }
+
+    protected Contact(Parcel in) {
+        this.mName = in.readString();
+        this.mPhoneNumber = in.readString();
+        this.mEmail = in.readString();
+        this.mImage = in.readInt();
+    }
+
+    public static final Parcelable.Creator<Contact> CREATOR = new Parcelable.Creator<Contact>() {
+        @Override
+        public Contact createFromParcel(Parcel source) {
+            return new Contact(source);
+        }
+
+        @Override
+        public Contact[] newArray(int size) {
+            return new Contact[size];
+        }
+    };
 }
